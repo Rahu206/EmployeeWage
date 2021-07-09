39d3b0b9253276a060925aeaf53f84d2cd7dc3c3
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-09 16:56:39 +0530

    Add files via upload

---
+++ Empwageuc.java
@@ -0,0 +1,35 @@
+
+public class Empwageuc {
+
+	public static final int is_Part_Time = 1;
+	public static final int is_Full_Time = 2;
+	public static final int no_of_Working_Days = 20;
+	
+	public static void main(String args[]){
+		
+			System.out.println("Welcome to EmpWage");
+			int totalEmpWage = 0;
+			int wagePerhr = 20;
+			int empHrs = 0;
+			int dailyWage = 0;
+			
+			for(int i=0; i<=no_of_Working_Days; i++){
+			
+			int check = (int) Math.floor(Math.random()*10)%3;
+			
+			switch(check){
+			case(is_Part_Time):
+				empHrs = 4;
+				break;
+			case(is_Full_Time):
+				empHrs = 8;
+				break;
+			default :
+				empHrs = 0;
+			}
+			dailyWage = empHrs * wagePerhr;
+			totalEmpWage += dailyWage;
+			}
+			System.out.println("Total EmpWage : "+totalEmpWage);
+	    }
+	}
