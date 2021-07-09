7941b0e3e824beb91e8a511afc2651a03b8dd5ad
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-09 16:27:12 +0530

    Add files via upload
    UC3 Added Part Time Wage

---
+++ Empwageuc.java
@@ -0,0 +1,25 @@
+
+public class Empwageuc {
+
+	public static void main(String args[]){
+			System.out.println("Welcome to EmpWage");
+			int wagePerhr = 20;
+			int empHrs = 0;
+			int dailyWage = 0;
+			int is_Part_Time = 1;
+			int is_Full_Time = 2;
+			
+			double check = Math.floor(Math.random()*10)%3;
+			
+			if(check == is_Full_Time){
+				empHrs = 8;
+			}else if(check == is_Part_Time){
+				empHrs = 4;
+			}
+			else{
+				empHrs = 0;
+			}
+			dailyWage = empHrs * wagePerhr;
+			System.out.println("EmpWage = " + dailyWage);
+		}
+	}
